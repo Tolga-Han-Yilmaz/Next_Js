1eaf34a42f413dc65d40fbc7bfc7324fa22e2ee0
Tolga-Han-Yilmaz <101406062+Tolga-Han-Yilmaz@users.noreply.github.com>
2023-04-05 14:20:03 +0300

    continue

---
+++ 02-rendering/app/page.js
@@ -0,0 +1,16 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [message, setMessage] = useState("initial");
+
+  useEffect(() => {
+    setMessage("use effect manipule etti")
+    console.log("useeffect");
+  }, []);
+  console.log("homepage");
+  return <div>Home Page
+<br/>
+    <h2>{message}</h2>
+  </div>;
+}
